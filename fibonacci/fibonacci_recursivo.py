511f61874c7158fe80665d865736b956e2e0e418
Luca Boing Albino <luca.boing@icloud.com>
2026-07-27 18:50:17 -0300

    ajustes finais fib recursivo

--- fibonacci/fibonacci_recursivo.py
+++ fibonacci/fibonacci_recursivo.py
@@ -1,29 +1,24 @@
-mensagem_erro = "A entrada deve ser um número inteiro maior ou igual a zero."
-
 def fib(n):
     # se for negativo, mostra o erro e para a funcao
     if n < 0:
-        print(mensagem_erro)
-        return
+        raise ValueError("A entrada deve ser um número inteiro maior ou igual a zero.")
 
-    # casos especiais
+    # casos base
     if n == 0:
         return 0
     if n == 1:
         return 1
 
-    resultado = fib(n-1) + fib(n-2)
-
-    return resultado
+    return fib(n-1) + fib(n-2)
 
 # --- entrada e saida ---
 try:
     numero = int(input("Digite um número: "))
-    resultado = fib(numero)
-    
-    # so imprime o resultado se ele nao for none (se nao deu erro)
-    if resultado is not None:
-        print(f"fib({numero}) = {resultado}")
-
 except ValueError:
-    print(mensagem_erro)
+    print("A entrada deve ser um número inteiro maior ou igual a zero.")
+else:
+    try:
+        resultado = fib(numero)
+        print(f"fib({numero}) = {resultado}")
+    except ValueError as erro:
+        print(erro)
