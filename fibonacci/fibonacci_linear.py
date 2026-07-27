9303c865e2ede180409bffb3d9e91c08d6420057
Luca Boing Albino <luca.boing@icloud.com>
2026-07-27 18:49:08 -0300

    ajustes finais fib linear

--- fibonacci/fibonacci_linear.py
+++ fibonacci/fibonacci_linear.py
@@ -1,10 +1,7 @@
-mensagem_erro = "A entrada deve ser um número inteiro maior ou igual a zero."
-
 def fib(n):
     # se for negativo, mostra o erro e para a funcao
     if n < 0:
-        print(mensagem_erro)
-        return
+        raise ValueError("A entrada deve ser um número inteiro maior ou igual a zero.")
 
     # casos especiais
     if n == 0:
@@ -26,11 +23,11 @@ def fib(n):
 # --- entrada e saida ---
 try:
     numero = int(input("Digite um número: "))
-    resultado = fib(numero)
-    
-    # so imprime o resultado se ele nao for none (se nao deu erro)
-    if resultado is not None:
-        print(f"fib({numero}) = {resultado}")
-
 except ValueError:
-    print(mensagem_erro)
+    print("A entrada deve ser um número inteiro maior ou igual a zero.")
+else:
+    try:
+        resultado = fib(numero)
+        print(f"fib({numero}) = {resultado}")
+    except ValueError as erro:
+        print(erro)
